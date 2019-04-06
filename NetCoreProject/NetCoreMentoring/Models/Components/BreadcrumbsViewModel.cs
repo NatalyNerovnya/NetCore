35194ea3a265592f9f7b58b976c4bbd88d78be76
Nataly Nerovnya <natanerovnja@gmail.com>
2019-04-06 14:21:43 +0300

    Add breadcrumbs view component

---
+++ NetCoreProject/NetCoreMentoring/Models/Components/BreadcrumbsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace NetCoreMentoring.Models.Components
+{
+    public class BreadcrumbsViewModel
+    {
+        public Dictionary<string, string> Breadcrumbs { get; set; }
+    }
+}
---
+++ NetCoreProject/NetCoreMentoring/ViewComponents/BreadcrumbsViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NetCoreMentoring.Models.Components;
+using System;
+using System.Collections.Generic;
+
+namespace NetCoreMentoring.ViewComponents
+{
+    public class BreadcrumbsViewComponent : ViewComponent
+    {
+        private const string defaultActionName = "Index";
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new BreadcrumbsViewModel()
+            {
+                Breadcrumbs = BuildBreadcrumbs()
+            };
+
+            return View(model);
+        }
+
+        private Dictionary<string, string> BuildBreadcrumbs()
+        {
+            var breadcrumbs = new Dictionary<string, string>();
+
+            var controller = ViewContext.RouteData.Values["controller"].ToString();
+            var action = ViewContext.RouteData.Values["action"].ToString();
+
+            breadcrumbs.Add(controller, Url.Action(defaultActionName, controller));
+
+            if (!action.Equals(defaultActionName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                breadcrumbs.Add(action, HttpContext.Request.Path + HttpContext.Request.QueryString);
+            }           
+
+            return breadcrumbs;
+        }
+    }
+}
