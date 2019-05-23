a697c499d082351443315771b6766e887107eefc
Natallia Nerovnya <Natallia_Nerovnya@epam.com>
2019-05-23 20:33:21 +0300

    TASK 5: Add administrator role

--- NetCoreProject/NetCoreMentoring/Areas/Identity/IdentityHostingStartup.cs
+++ NetCoreProject/NetCoreMentoring/Areas/Identity/IdentityHostingStartup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace NetCoreMentoring.Areas.Identity
                         context.Configuration.GetConnectionString("AccountContextConnection")));
 
                 services.AddDefaultIdentity<User>()
+                    .AddRoles<IdentityRole>()
                     .AddEntityFrameworkStores<AccountContext>();
             });
         }
---
+++ NetCoreProject/NetCoreMentoring/Controllers/AdministratorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMentoring.Areas.Identity.Data;
+
+namespace NetCoreMentoring.Controllers
+{
+    [Authorize(Roles = Role.Admin)]
+    [Route("admin")]
+    public class AdministratorController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AdministratorController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+
+            return View(users);
+        }
+    }
+}
